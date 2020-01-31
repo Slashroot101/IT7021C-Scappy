7e10bb2be9a59f65808c6093c4abb3c77fff4006
Samuel Curry <samc735@gmail.com>
2020-01-30 23:17:55 -0500

    WIP

---
+++ packets.py
@@ -0,0 +1,9 @@
+from scapy.all import *
+
+for n in range(10):
+	srcIP="172.16.16."+str(RandNum(1, 254))
+	destIP="192.168.5.5"
+	ipFlags=0 if((n+1)%5) else 4
+	payload="Bad" if((n+1)%2) else "Good"
+	ip=IP(src=srcIP, flags=ipFlags, dst=destIP, ttl=160)
+	send(ip/TCP()/Raw(load=payload))
