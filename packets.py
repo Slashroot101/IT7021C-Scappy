560ea43c3a57113f6774497df7fa60aaecd52258
Samuel Curry <samc735@gmail.com>
2020-02-01 11:45:03 -0500

    set evil bit set

--- packets.py
+++ packets.py
@@ -3,7 +3,7 @@ from scapy.all import *
 for n in range(10):
 	srcIP="172.16.16."+str(RandNum(1, 254))
 	destIP="192.168.5.5"
-	ipFlags=0 if((n+1)%5) else 4
+	ipFlags=0x02 if((n+1)%5) else 0x8
 	payload="Bad" if((n+1)%2) else "Good"
 	ip=IP(src=srcIP, flags=ipFlags, dst=destIP, ttl=160)
-	send(ip/TCP()/Raw(load=payload))
+	send(ip/TCP(dport=RandNum(1000, 1009))/Raw(load=payload))
